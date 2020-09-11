e7f861ddf006a93ceb9df18f70ba2dfd49085a2c
Steven Ly <1stevenly92@gmail.com>
2020-09-11 09:53:07 -0700

    added components

---
+++ kombucha/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <h1>Most boring Kombuncha Store!</h1>
+    )
+}
+
+export default Header;
---
+++ kombucha/src/components/KombunchaControl.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+class KombunchaControl extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            formVisibleOnPage: false
+        }
+    }
+}
+
+
+
