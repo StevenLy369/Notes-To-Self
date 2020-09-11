6696e56316acfe2251547e3fb6e1dc633fb46844
Steven Ly <1stevenly92@gmail.com>
2020-09-11 10:05:17 -0700

    got header to show and working on kombunchaList/kombuncha

---
+++ kombucha/src/components/KombunchaList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Kombuncha from './Kombuncha'
+import PropTypes from "prop-types"
+
+function KombunchaList(props) {
+    return (
+        <React.Fragment>
+            {props.KombunchaList.Map((kombuncha,index) =>
+            <Kombuncha name={kombuncha.name}
+                brand = {kombuncha.brand}
+                price = {kombuncha.price}
+                flavor = {kombuncha.flavor}
+                key ={index} />
+                
+                
+                
+                )} 
+
+
+        </React.Fragment>
+    )
+
+
+}
+KombunchaList.propTypes = {
+    kombunchaList: PropTypes.array
+}
+
+
+export default KombunchaList;
