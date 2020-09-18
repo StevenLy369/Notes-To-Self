0f16ded8a3208ea9a2b02e7d5dac8522461ee3df
Steven Ly <1stevenly92@gmail.com>
2020-09-18 14:02:11 -0700

    working on writing reducers for adding kombuncha

--- kombucha/src/_tests_/reducers/index-reducer.test.js
+++ kombucha/src/_tests_/reducers/index-reducer.test.js
@@ -2,17 +2,31 @@ import rootReducer from '../../components/reducers/index';
 
 import formVisibleReducer from '../../components/reducers/form-visible-reducer';
 import kombunchaListReducer from '../../components/reducers/kombuncha-list-reducer';
+import Kombuncha from '../../components/Kombuncha';
 
 
 
 describe("rootReducer", () => {
     //Test one
-
+    let action;
     test('Should return default state if no action type is reconginzed', () => {
-        expect(kombunchaListReducer({} , {type:null})).toEqual({})
+        expect(kombunchaListReducer({}, { type: null })).toEqual({})
     })
 
 
 
-    
-})
+    //Test two
+    test('Should successfully add new ticket data to masterTicketList', () => {
+        
+        action = {
+            type: 'ADD_KOMBUNCHA',
+            brand: "coke",
+            price: "5",
+            flavor: "cherry",
+            stock: 1,
+        }
+        expect(kombunchaListReducer({}, action)).toEqual({ [0]: {brand: "coke",price: "5",flavor: "cherry",stock: "1", }})
+    })
+
+
+});
--- kombucha/src/components/actions/index.js
+++ kombucha/src/components/actions/index.js
@@ -3,3 +3,16 @@ import * as c from './actionTypes';
 export const toggleForm = () => ({
 	type: c.TOGGLE_FORM
 });
+
+
+export const addKombuncha = (kombuncha) => {
+    const {brand,price,flavor,stock,id} = kombuncha;
+    return{
+        type:c.ADD_KOMBUNCHA,
+        brand:brand,
+        price:price,
+        flavor:flavor,
+        stock:stock,
+        id:id
+    }
+}
